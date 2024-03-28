736374e86499dce3012d2dd25f7e63f34148d996
Prashant_Farne <farneprashant707@gmail.com>
2024-03-28 11:13:31 +0530

    Integrate loader component

--- src/pages/userList.tsx
+++ src/pages/userList.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import Header from "../component/header";
 import "./style.css";
 import Sidebar from "./sidebar";
+import Loader from "../component/loader";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
@@ -27,68 +28,72 @@ const UserList = () => {
       <div>
         <Header title="User List" />
         <div className="overflow-x-auto" style={{ marginLeft: "10px" }}>
-          <table className="table-auto w-full bg-custom">
-            <thead>
-              <tr className="bg-gray-200">
-                <th className="px-6 py-3 custom-width">ID</th>
-                <th className="px-6 py-3 custom-width">First Name</th>
-                <th className="px-6 py-3 custom-width">Last Name</th>
-                <th className="px-6 py-3 custom-width">Full Name</th>
-                <th className="px-6 py-3 custom-width">Email</th>
-                <th className="px-6 py-3 custom-width">Avatar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users &&
-                users.map((user: any, index) => (
-                  <tr
-                    key={index}
-                    className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}
-                  >
-                    <td
-                      className="px-6 py-3 custom-width text-center"
-                      style={{ textAlign: "center", verticalAlign: "middle" }}
+          {users?.length ? (
+            <table className="table-auto w-full bg-custom">
+              <thead>
+                <tr className="bg-gray-200">
+                  <th className="px-6 py-3 custom-width">ID</th>
+                  <th className="px-6 py-3 custom-width">First Name</th>
+                  <th className="px-6 py-3 custom-width">Last Name</th>
+                  <th className="px-6 py-3 custom-width">Full Name</th>
+                  <th className="px-6 py-3 custom-width">Email</th>
+                  <th className="px-6 py-3 custom-width">Avatar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users &&
+                  users.map((user: any, index) => (
+                    <tr
+                      key={index}
+                      className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}
                     >
-                      {user.id}
-                    </td>
-                    <td
-                      className="px-6 py-3 custom-width text-center"
-                      style={{ textAlign: "center", verticalAlign: "middle" }}
-                    >
-                      {user.first_name}
-                    </td>
-                    <td
-                      className="px-6 py-3 custom-width text-center"
-                      style={{ textAlign: "center", verticalAlign: "middle" }}
-                    >
-                      {user.last_name}
-                    </td>
-                    <td
-                      className="px-6 py-3 custom-width text-center"
-                      style={{ textAlign: "center", verticalAlign: "middle" }}
-                    >
-                      {user.first_name} {user.last_name}
-                    </td>
-                    <td
-                      className="px-6 py-3 custom-width text-center"
-                      style={{ textAlign: "center", verticalAlign: "middle" }}
-                    >
-                      {user.email}
-                    </td>
-                    <td
-                      className="px-6 py-3 custom-width text-center"
-                      style={{ textAlign: "center", verticalAlign: "middle" }}
-                    >
-                      <img
-                        src={user.avatar}
-                        alt={`Avatar of ${user.first_name} ${user.last_name}`}
-                        className="h-10 w-10 rounded-full"
-                      />
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+                      <td
+                        className="px-6 py-3 custom-width text-center"
+                        style={{ textAlign: "center", verticalAlign: "middle" }}
+                      >
+                        {user.id}
+                      </td>
+                      <td
+                        className="px-6 py-3 custom-width text-center"
+                        style={{ textAlign: "center", verticalAlign: "middle" }}
+                      >
+                        {user.first_name}
+                      </td>
+                      <td
+                        className="px-6 py-3 custom-width text-center"
+                        style={{ textAlign: "center", verticalAlign: "middle" }}
+                      >
+                        {user.last_name}
+                      </td>
+                      <td
+                        className="px-6 py-3 custom-width text-center"
+                        style={{ textAlign: "center", verticalAlign: "middle" }}
+                      >
+                        {user.first_name} {user.last_name}
+                      </td>
+                      <td
+                        className="px-6 py-3 custom-width text-center"
+                        style={{ textAlign: "center", verticalAlign: "middle" }}
+                      >
+                        {user.email}
+                      </td>
+                      <td
+                        className="px-6 py-3 custom-width text-center"
+                        style={{ textAlign: "center", verticalAlign: "middle" }}
+                      >
+                        <img
+                          src={user.avatar}
+                          alt={`Avatar of ${user.first_name} ${user.last_name}`}
+                          className="h-10 w-10 rounded-full"
+                        />
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          ) : (
+            <Loader />
+          )}
         </div>
       </div>
     </div>
