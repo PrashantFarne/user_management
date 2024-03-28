db06cc362b6de89b83509e93761d070b406af4be
Prashant_Farne <farneprashant707@gmail.com>
2024-03-28 11:13:08 +0530

    Changes in css

--- src/pages/sidebar.tsx
+++ src/pages/sidebar.tsx
@@ -4,8 +4,8 @@ const Sidebar = () => {
   return (
     <div className="flex justify-center custom-sidebar-bg">
       <div className="bg-gray-800 h-screen w-64">
-        <div className="p-4">
-          <div>User</div>
+        <div className="p-4" style={{ padding: "40px" }}>
+          <div className="font-bold">User</div>
         </div>
       </div>
     </div>
