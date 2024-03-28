adad8567bf509385e8b7f2139b878e0ae87bf47c
Prashant_Farne <farneprashant707@gmail.com>
2024-03-28 13:24:30 +0530

    Remove comments

--- src/pages/userList.test.tsx
+++ src/pages/userList.test.tsx
@@ -10,7 +10,6 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("UserList component", () => {
   test("renders user list correctly", async () => {
-    // Mock response data
     const mockApiResponse: any = {
       data: [
         {
@@ -30,25 +29,19 @@ describe("UserList component", () => {
       ],
     };
 
-    // Mock axios.get to resolve with mockData
     mockedAxios.get.mockResolvedValueOnce(mockApiResponse);
 
-    // Render UserList component
     render(<UserList />);
 
-    // Wait for the user data to be loaded
     await waitFor(() => {
       expect(axios.get).toHaveBeenCalledTimes(1);
     });
 
-    // Check if the header is rendered correctly
     expect(screen.getByText("User List")).toBeInTheDocument();
 
-    // Check if the user list table is rendered correctly
     const idColumnHeaders = screen.getAllByText("ID");
     expect(idColumnHeaders).toHaveLength(1);
 
-    // Check if the user data is rendered correctly
     expect(screen.getByText("John")).toBeInTheDocument();
     expect(screen.getByText("Doe")).toBeInTheDocument();
     expect(screen.getByText("john@example.com")).toBeInTheDocument();
@@ -56,13 +49,10 @@ describe("UserList component", () => {
   });
 
   test("renders loader while fetching data", async () => {
-    // Mock axios.get to simulate loading state
     mockedAxios.get.mockImplementationOnce(() => new Promise(() => {}));
 
-    // Render UserList component
     render(<UserList />);
 
-    // Check if loader is rendered
     expect(screen.getByTestId("loader")).toBeInTheDocument();
   });
 });
